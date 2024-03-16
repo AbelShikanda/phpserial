0df943027886e0829185458cb225bdf319f771bb
abel <abelshikanda3@dmail.com>
2024-03-16 15:29:03 +0300

    added admin, access logs, countdown, notifications -m -c -r and mailer

---
+++ app/Http/Controllers/AccessLogsController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\AccessLogs;
+use Illuminate\Http\Request;
+
+class AccessLogsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\AccessLogs  $accessLogs
+     * @return \Illuminate\Http\Response
+     */
+    public function show(AccessLogs $accessLogs)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\AccessLogs  $accessLogs
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(AccessLogs $accessLogs)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\AccessLogs  $accessLogs
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, AccessLogs $accessLogs)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\AccessLogs  $accessLogs
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(AccessLogs $accessLogs)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/CountdownController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Countdown;
+use Illuminate\Http\Request;
+
+class CountdownController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Countdown  $countdown
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Countdown $countdown)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Countdown  $countdown
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Countdown $countdown)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Countdown  $countdown
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Countdown $countdown)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Countdown  $countdown
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Countdown $countdown)
+    {
+        //
+    }
+}
